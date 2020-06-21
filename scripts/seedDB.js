bc511e6d2d51746b1ce57da4124e35379a5932e1
erinruthmaness <erinruthmaness@yahoo.com>
2020-06-21 09:28:59 -0400

    initial folder structure and test data

---
+++ scripts/seedDB.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Users collection and inserts the user below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/reactreadinglist"
+);
+
+const userSeed = [
+  {
+    username: "admin",
+    password: "admin",
+    email: "lesmisloony@yahoo.com",
+    date: new Date(Date.now())
+  }
+]
+
+db.User
+  .remove({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
---
+++ xclient/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Users from ' ./pages/Users';
+
+function App() {
+  return (
+    <div>initial commit</div>
+  );
+}
+
+export default App;
